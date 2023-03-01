e939276964fb828271255cbebe1267ac224417e7
Mindespa <mindaugas.strong@gmail.com>
2023-03-01 18:28:44 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+ 
+
+  <?php
+
+                              //  3. Ciklai
+
+// 1 Naršyklėje nupieškite linija iš 400 “*”. 
+// Naudodami css stilių “suskaldykite” liniją taip, kad visos žvaigždutės matytųsi ekrane;
+// Programiškai “suskaldykite” žvaigždutes taip, kad vienoje eilutėje nebūtų daugiau nei 50 “*”; 
+                                                          
+
+echo "<p>" . "1 Udavinys ". "</p>";
+
+
+
+?>
+
+</body>
+</html>
